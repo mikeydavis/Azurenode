f67f3f8dc74c9e370d52a200f78eba03012dfc9e
mikeydavis <mikiedavis@hotmail.com>
2019-06-13 10:12:54 +0100

    Update environment.js

--- env/environment.js
+++ env/environment.js
@@ -2,7 +2,7 @@
 module.exports = {
     accountName: 'mean-demo-cosmosdb',
     databaseName: 'familiesdb', 
-    key: 'kcGCpQWpsUAJ3dc63RNbfkYMtxXI1wPC6hYbb0SLmCH9ALNY6GNaq1FEDlI51oZQekPwZUu3G3DCnjLKIr0Oaw%3D%3D',
+    key: '',
     //key:'yUndgPV4JN8ZPINVBxCK7c6W1cOqjX1EsXqaPFWRb5wN6qKIoMT51NdE0J1ayBsFKrVUzw9QpRFG8fL6mEWttQ=='
     port: 10255
   };
